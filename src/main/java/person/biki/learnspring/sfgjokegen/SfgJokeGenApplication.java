f5de13f53828a1d301018b4bd4fe0592111a8029
Bikram Kashyap <kashyap.bikram@gmail.com>
2021-05-22 15:23:56 -0400

    Initial commit

---
+++ src/main/java/person/biki/learnspring/sfgjokegen/SfgJokeGenApplication.java
@@ -0,0 +1,13 @@
+package person.biki.learnspring.sfgjokegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgJokeGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgJokeGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/person/biki/learnspring/sfgjokegen/SfgJokeGenApplicationTests.java
@@ -0,0 +1,13 @@
+package person.biki.learnspring.sfgjokegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJokeGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
